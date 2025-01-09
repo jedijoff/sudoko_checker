ee64a570ed67460ba5b97394b9a1fba01d466a7b
Jon Watts <jonathanwatts@mac.com>
2025-01-09 17:32:34 +0000

    Update sudoku_checker.py

--- sudoku_checker.py
+++ sudoku_checker.py
@@ -1,86 +1,81 @@
-user_grid = []
-check_list = ['1','2','3','4','5','6','7','8','9']
-results = {'rows': False, 'columns': False, 'blocks': False}
-blocks_of_three_rows = ()
-
-
-def check_input(row, i=0):
-    # This function is now working as expected do not alter!
-    global user_grid
-
-    if len(row) != 9:
-        row = input(f"Please enter row {i + 1} again: ")
-        check_input(row, i)
-    else:
-        user_grid.append(row)
-        return
-
-
 def grid_maker():
     # This function is now working as expected do not alter!
-    global blocks_of_three_rows
+    blocks_of_three_rows = []
+    user_grid = []
 
     for i in range(9):
         row = input(f"Enter row {i+1}: ")
         check_input(row, i)
+        user_grid.append(row)
 
-    blocks_of_three_rows = ((user_grid[0],
-                            user_grid[1],
-                            user_grid[2]),
-
-                            (user_grid[3],
-                            user_grid[4],
-                            user_grid[5]),
-
-                            (user_grid[6],
-                            user_grid[7],
-                            user_grid[8]))
     check_rows(user_grid)
+
+
+def check_input(row, i=0):
+    # This function is now working as expected do not alter!
+    if len(row) != 9:
+        row = input(f"Please enter row {i + 1} again: ")
+        check_input(row, i)
+    else:
+        return
 
 
 def check_rows(user_grid):
     # This function is now working as expected do not alter!
-    global results
-    global check_list
+    user_grid = user_grid
     rows_result = 0
+    results = {}
+    check_list = ['1','2','3','4','5','6','7','8','9']
 
     for i in range(9):
-        if sorted(user_grid[i]) == check_list:
+        user_grid[i] = list(user_grid[i])
+
+    for row in user_grid:
+        if sorted(row) == check_list:
             rows_result += 1
-            check_squares(user_grid)
 
-        if rows_result == 9:
-            results['rows'] = True
-        else:
-            results['rows'] = False
+    if rows_result == 9:
+        results['rows'] = True
+    else:
+        results['rows'] = False
+    print(f'Row results{results}')
+    check_squares(user_grid, results, check_list)
 
-    check_squares(user_grid)
 
-
-def check_squares(user_grid):
+def check_squares(user_grid, results, check_list):
     # This function is now working as expected do not alter!
-    global check_list
-    global results
+    print(f'results received {results}')
     check_count = 0
+    blocks_of_3_rows = [[user_grid[0],
+                         user_grid[1],
+                         user_grid[2]],
+
+                        [user_grid[3],
+                         user_grid[4],
+                         user_grid[5]],
+
+                        [user_grid[6],
+                         user_grid[7],
+                         user_grid[8]]]
 
     for i in range(3):
         sq_1 = []
         sq_2 = []
         sq_3 = []
         for j in range(0, 3):
-            sq_1.append(blocks_of_three_rows[i][0][j])
-            sq_1.append(blocks_of_three_rows[i][1][j])
-            sq_1.append(blocks_of_three_rows[i][2][j])
+            sq_1.append(blocks_of_3_rows[i][0][j])
+            sq_1.append(blocks_of_3_rows[i][1][j])
+            sq_1.append(blocks_of_3_rows[i][2][j])
 
         for j in range(3, 6):
-            sq_2.append(blocks_of_three_rows[i][0][j])
-            sq_2.append(blocks_of_three_rows[i][1][j])
-            sq_2.append(blocks_of_three_rows[i][2][j])
+            sq_2.append(blocks_of_3_rows[i][0][j])
+            sq_2.append(blocks_of_3_rows[i][1][j])
+            sq_2.append(blocks_of_3_rows[i][2][j])
 
         for j in range(6, 9):
-            sq_3.append(blocks_of_three_rows[i][0][j])
-            sq_3.append(blocks_of_three_rows[i][1][j])
-            sq_3.append(blocks_of_three_rows[i][2][j])
+            sq_3.append(blocks_of_3_rows[i][0][j])
+            sq_3.append(blocks_of_3_rows[i][1][j])
+            sq_3.append(blocks_of_3_rows[i][2][j])
 
         if check_list == sorted(sq_1) and sorted(sq_2) and sorted(sq_3):
             check_count += 1
@@ -89,8 +84,8 @@ def check_squares(user_grid):
             results['blocks'] = True
         else:
             results['blocks'] = False
-
-    check_columns(user_grid)
+    print(f'Blocks results: {results}')
+    check_columns(user_grid, results, check_list)
 
 
 """The following commented out lines can safely be deleted.
@@ -119,25 +114,29 @@ the diagonals of a 9x9 grid, represented by 9 tuples"""
 #        results['diagonals'] = False
 
 
-def check_columns(user_grid):
+def check_columns(user_grid, results, check_list):
     # I have got to write this function.
-    global check_list
-    global results
+    print(f'results received {results}')
     column_result = 0
 
     for i in range(9):
         column = []
         for j in range (9):
-            column.append(user_grid[j][i])
+                column.append(user_grid[j][i])
         if sorted(column) == check_list:
             column_result += 1
 
     if column_result == 9:
         results['columns'] = True
+    else:
+        results['columns'] = False
+
+    print(f'column results: {results}')
+    check_results(results)
 
 
-def check_results():
-    global results
+def check_results(results):
+    print(f'results received {results}')
     if results['rows'] and results['blocks'] and results['columns']:
         print('You have completed the sudoku puzzle correctly.')
     else:
@@ -145,4 +144,3 @@ def check_results():
 
 
 grid_maker()
-check_results()
